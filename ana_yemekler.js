fe16d661ac4bd48f2e295bfa61a379f174fe50c7
root <root@DESKTOP-F1QO2PI.localdomain>
2024-10-26 17:09:34 +0300

    index.html son halinde ve ana_yemekler.html tablolaştırıldı

---
+++ ana_yemekler.js
@@ -0,0 +1,28 @@
+// Tema değiştirme butonuna tıklama olayını dinleyin
+document.getElementById("light").addEventListener("click", function() {
+    const body = document.body;
+    const themeIcon = document.getElementById("themeIcon");
+    const circle = document.querySelector(".light.circle");
+
+    if (body.classList.contains("light-mode")) {
+        // Karanlık moda geçiş
+        body.classList.remove("light-mode");
+        body.classList.add("dark-mode");
+        themeIcon.classList.remove("fa-sun");
+        themeIcon.classList.add("fa-moon");
+
+        // Çemberin içini siyah yap, ay ikonunu beyaz yap
+        circle.style.backgroundColor = "black";
+        themeIcon.style.color = "white";
+    } else {
+        // Aydınlık moda geçiş
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+        themeIcon.classList.remove("fa-moon");
+        themeIcon.classList.add("fa-sun");
+
+        // Çemberin içini beyaz yap, güneş ikonunu siyah yap
+        circle.style.backgroundColor = "white";
+        themeIcon.style.color = "black";
+    }
+});
